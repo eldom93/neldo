5fcc1d7da94a3290539c6212088cac55334121b0
pharoh-dev <nyera.a.e@gmail.com>
2020-02-25 22:26:39 -0600

    add:UniqueEmailAddresses

---
+++ UniqueEmailAddresses/unique-email-addresses.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string[]} emails
+ * @return {number}
+ */
+var numUniqueEmails = function(emails) {
+  //declare variables
+  let emailList = [];
+  let localName = "";
+  let domainName = "";
+  let forwardedCopyEmailList = [];
+  let filteredEmailList = [];
+
+  //emaillist regex validation
+
+  //email list item regex vaildation
+
+  //localname regex validation
+
+  //domainname regex validation
+
+  //define the forwarededCopyEmailList
+
+  //make changes to email list
+
+  //check and reduce list for duplicates within the email list
+
+  //return the length of new filteredEmailList
+};
